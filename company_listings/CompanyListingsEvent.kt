2d4acfdd6aa6662e691479cd85fae77d6e29c1f2
cmulholland11 <91726483+cmulholland11@users.noreply.github.com>
2022-06-22 13:09:16 +0100

    Delete company_listings directory

--- company_listings/CompanyListingsEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.plcoding.stockmarketapp.presentation.company_listings
-
-sealed class CompanyListingsEvent {
-    object Refresh: CompanyListingsEvent()
-    data class OnSearchQueryChange(val query: String): CompanyListingsEvent()
-
-}
